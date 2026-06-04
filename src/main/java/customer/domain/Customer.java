e052dc4255ba44d2c0de60c9575a9df03d47e927
tmdgus0245 <tmdgus0245@ajou.ac.kr>
2026-06-04 21:50:55 +0900

    chore: 도메인 기반 패키지 구조 생성

---
+++ src/main/java/customer/domain/Customer.java
@@ -0,0 +1,45 @@
+package customer.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.management.relation.Role;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phone;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public Customer(String email, String password, String name, String phone) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/customer/infra/CustomerRepository.java
@@ -0,0 +1,11 @@
+package customer.infra;
+
+import customer.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    boolean existsByEmail(String email);
+    Optional<Customer> findByEmail(String email);
+}
